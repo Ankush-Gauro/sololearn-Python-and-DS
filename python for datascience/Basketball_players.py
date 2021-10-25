d75ba2cc53010dbd86fcf97866c7e8cc01528a7a
AnkushGauro <72354882+Ankush-Gauro@users.noreply.github.com>
2021-10-25 19:32:47 +0545

    Add files via upload

---
+++ python for datascience/Basketball_players.py
@@ -0,0 +1,29 @@
+"""
+The given code includes a list of heights for various basketball players.
+You need to calculate and output how many players are in the range of one standard deviation from the mean.
+"""
+def mean(array): 
+    x=0
+    for j in array:
+        x+=j
+    return x/len(array)
+    
+def std(a):  
+    sum=0
+    for j in a:
+        sum+=j**2
+    y = abs(mean(a)**2 - sum/len(a))
+    return y**0.5
+
+def rstd(a): 
+    ans=0
+    m = mean(a)
+    st = std(a)
+    for x in players:
+        if (m-st) <= x <= (m+st):
+            ans+=1
+    return ans
+    
+    
+players = [180, 172, 178, 185, 190, 195, 192, 200, 210, 190]
+print(rstd(players))
---
+++ python for datascience/House_Prices.py
@@ -0,0 +1,18 @@
+"""
+You are given an array that represents house prices. 
+Calculate and output the percentage of houses that are within one standard deviation from the mean.
+"""
+import numpy as np
+
+data = np.array([150000, 125000, 320000, 540000, 200000, 120000, 160000, 230000, 280000, 290000, 300000, 500000, 420000, 100000, 150000, 280000])
+m = np.mean(data)
+std = np.std(data)
+def rstd(a):
+    ans = 0
+    for x in data:
+        if (m-std) <= x <= (m + std):
+            ans += 1
+    return ans
+no = rstd(data)
+perctage = (no/len(data))*100
+print(perctage)
